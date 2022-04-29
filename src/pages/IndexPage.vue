39e9b180887706c4a61c83b05b5a31cea6da6ccd
Leonardo <leonard296@live.com>
2022-04-29 11:11:24 -0300

    create left nav bar

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,5 +1,23 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page>
+    <div class="row">
+
+      <div class="col-2">
+        <div class="leftNavBar">
+          <div class="transformLogo">
+            <q-img src="https://leodev7.github.io/portfolio/img/leo-logo.png" width="10rem" />
+          </div>
+          <p>Email</p>
+        </div>
+      </div>
+
+      <div class="col-10">
+        <div class="container">
+          <h1>Olá, eu sou o leonardo!</h1>
+        </div>
+      </div>
+
+    </div>
   </q-page>
 </template>
 
