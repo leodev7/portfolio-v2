2d93f72c1c0a9cc7f1851ac72986ffb6d7ca6eac
Leonardo <leonard296@live.com>
2022-04-29 19:36:36 -0300

    create nav bar

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -12,6 +12,19 @@
       </div>
 
       <div class="col-10">
+        <nav style="display: flex">
+          <ul class="q-col-gutter-md q-mt-xs" style="display: flex; list-style: none">
+            <li v-for="page in pages" :key="page.id">
+              <a target="_blank" :href="page.route" style="text-decoration: none; color: white">{{ page.name }}</a>
+            </li>
+          </ul>
+          <q-space />
+          <ul class="q-col-gutter-md q-mt-xs q-mr-lg" style="display: flex; list-style: none; color: white">
+            <li v-for="social in socials" :key="social.id">
+              <q-btn :icon="social.icon" :href="social.url" target="_blank" />
+            </li>
+          </ul>
+        </nav>
         <div class="container">
           <h1>Olá, eu sou o leonardo!</h1>
         </div>
@@ -24,6 +37,23 @@
 <script>
 
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  data () {
+    return {
+      pages: [
+        { name: 'home', route: 'home' },
+        { name: 'about me', route: 'about-me' },
+        { name: 'blog', route: 'blog' },
+        { name: 'portfolio', route: 'portfolio' }
+      ],
+
+      socials: [
+        { icon: 'currency_bitcoin', url: 'https://github.com/leodev7/' },
+        { icon: 'link', url: 'https://www.linkedin.com/in/leonardodev7/' },
+        { icon: 'telegram', url: 'https://t.me/Leop7' },
+        { icon: 'photo_camera', url: 'https://instagram.com/leonardodev7' }
+      ]
+    }
+  }
 }
 </script>
